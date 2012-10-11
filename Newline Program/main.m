432e69fd0380577e1dc9371031199974a74a1398
Keith <user@Keiths-Macbook-Pro.local>
2012-10-11 16:22:22 -0700

    Initial Commit

---
+++ Newline Program/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Newline Program
+//
+//  Created by Keith on 10/11/12.
+//  Copyright (c) 2012 Appology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
